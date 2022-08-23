11175d3fac3f5df35240d018232a18fa3a351b9f
ShilpaItnal23 <saishilpa723@gmail.com>
2022-08-23 14:21:14 +0530

    commit-msg

---
+++ src/main/java/in/ashokit/MiniProjectJrtpApplication.java
@@ -0,0 +1,14 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectJrtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectJrtpApplication.class, args);
+		System.out.println("Done ................!!!!!");
+	}
+
+}
